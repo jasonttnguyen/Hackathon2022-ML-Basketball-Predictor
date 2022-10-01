4a2eb496f9bfce046b85fa022d874baeb3b52705
jasonttnguyen <67293576+jasonttnguyen@users.noreply.github.com>
2022-10-01 16:38:02 -0400

    Update PlayerStatPredict.py

--- PlayerStatPredict.py
+++ PlayerStatPredict.py
@@ -1,3 +1,4 @@
+from audioop import avg
 import sklearn
 import numpy
 import pandas
@@ -5,11 +6,10 @@ import sklearn
 from sklearn import linear_model
 from datetime import timedelta
 
-from sklearn.metrics import accuracy_score
 
 
-#This program predicts the stats of the current star raptor players as of Oct 30, 2019 (using linear regression algorithm)
-#This formats all the data so the most recent game is on the right, and last played game is on the left, as well as formatting the data given so it is easier to work with
+#This program predicts the stats of a given player with their csv file from basketball-reference.com (using linear regression algorithm)
+
 playerData = pandas.read_csv("vanvleet2021.csv")
 
 playerData = playerData[playerData["MP"].str.contains("Inactive") == False]
@@ -17,26 +17,20 @@ playerData = playerData[playerData["MP"].str.contains("Did Not Play") == False]
 playerData = playerData[playerData["MP"].str.contains("Did Not Dress") == False]
 playerData = playerData[playerData["FT%"].str.contains("NaN") == False]  #need to shift the games played/rnk down
 
-playerData = playerData[["MP","FG","FGA","FG%","3P","3PA","3P%","FT","FTA","FT%","ORB","DRB","TRB","AST","STL","BLK","TOV","PF","PTS","+/-"]]
+playerData = playerData[["MP","FG","FGA","3P","3PA","FT","FTA","ORB","DRB","TRB","AST","STL","BLK","TOV","PF","PTS","+/-"]]
 
 def seconder(x):  #get seconds for minutes played
 	mins, secs = map(float, x.split(':'))
 	td = timedelta(minutes=mins, seconds=secs)
 	return td.total_seconds()
-playerData['MP'] = playerData['MP'].apply(seconder)
-
+playerData['MP'] = playerData['MP'].apply(seconder)  #Take in mind, MP is not minutes played and actually seconds played
 
 
 predictionVar = "PTS"
 
-print("DEBUG HERE") #Debug###########################################
 x = numpy.array(playerData.drop([predictionVar],1))  #creates an array of arrays containing the variables
 y = numpy.array(playerData[predictionVar])           #creates an array of the wanted variable
-print("This is x: \n") #Debug###########################################
-print(x)               #Debug###########################################
 
-print("This is y: \n")  #Debug###########################################
-print(y)
 
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1 )  #trains model with 90 percent of data and test on 10% of data
 
@@ -47,7 +41,28 @@ linear.fit(x_train,y_train)  #fit model with the training data
 accu = linear.score(x_test,y_test)  # used to print accuracy score
 print("The accuracy is: "+ str(accu))
 
-predictions = linear.predict(x_test)  #predict using x_test, which is randomized by sklearn
-print(len(predictions)) #Debug###########################################
-for i in range(len(predictions)):
-    print(predictions[i],x_test[i],y_test[i])
+#["MP","FG","FGA","FG%","3P","3PA","3P%","FT","FTA","FT%","ORB","DRB","TRB","AST","STL","BLK","TOV","PF","PTS","+/-"]
+avgMP=playerData["MP"].mean()
+avgFG=playerData["FG"].mean()
+avgFGA=playerData["FGA"].mean()
+avg3P=playerData["3P"].mean()
+avg3PA=playerData["3PA"].mean()
+avgFT=playerData["FT"].mean()
+avgFTA=playerData["FTA"].mean()
+avgORB=playerData["ORB"].mean()
+avgDRB=playerData["DRB"].mean()
+avgTRB=playerData["TRB"].mean()
+avgAST=playerData["AST"].mean()
+avgSTL=playerData["STL"].mean()
+avgBLK=playerData["BLK"].mean()
+avgTOV=playerData["TOV"].mean()
+avgPF=playerData["PF"].mean()
+avgPTS=playerData["PTS"].mean()
+avgPM=playerData["+/-"].sum()
+
+averageStat = pandas.DataFrame({"MP":[avgMP],"FG":[avgFG],"FGA":[avgFGA],"3P":[avg3P],"3PA":[avg3PA],"FT":[avgFT],"FTA":[avgFTA],"ORB":[avgORB],"DRB":[avgDRB],"TRB":[avgTRB],"AST":[avgAST],"STL":[avgSTL],"BLK":[avgBLK],"TOV":[avgTOV],"PF":[avgPF],"PTS":[avgPTS],"+/-":[avgPM]})
+
+print(averageStat)
+#predictions = linear.predict(x_test)  #predict using x_test, which is randomized by sklearn
+#for i in range(len(predictions)):
+#    print(predictions[i],x_test[i],y_test[i])
